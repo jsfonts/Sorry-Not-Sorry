ba6f14bfd57551ea7cc23522fe723c9a038b3afa
Maddie <bramson@MaddiesLaptop.>
2024-12-02 19:46:34 -0500

    added background music

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,13 @@
 import controllers.GameController;
 import views.MainMenu;
+import models.BackgroundMusicPlayer;
 
-public class Main{
-    public static void main(String [] args){
+public class Main {
+    public static void main(String[] args) {
+        String musicFilePath = "../resources/BackgroundMusic.wav";
+        BackgroundMusicPlayer musicPlayer = new BackgroundMusicPlayer();
+        musicPlayer.play(musicFilePath); 
+        
         GameController gc = new GameController();
         MainMenu mainMenu = new MainMenu(gc);
         mainMenu.setLocationRelativeTo(null);
